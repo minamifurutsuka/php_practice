4f5bc33bfb8cdfd8901968f7b66a62b09f8a2482
minami <sec0415.minami@gmail.com>
2024-03-12 11:54:38 +0000

    コメント追加

--- practice.php
+++ practice.php
@@ -1,16 +1,20 @@
 <?php
 echo 'hello php!';
 echo "\n";
-$a=3;
-$b=7;
-echo $a+$b;
+
+$a = 3;
+$b = 7;
+echo $a + $b;
 echo "\n";
-$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
 echo $array_month[7];
 echo "\n";
-$hello="Hello,";
-$name="minami";
-$world="‘s World!";
+
+$hello = "Hello,";
+$name = "minami";
+$world = "‘s World!";
 echo $hello . $name . $world;
 echo "\n";
 
+# 課題４
