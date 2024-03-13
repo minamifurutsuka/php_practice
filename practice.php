ad8f81ade0019af8eb743c0e1c752533cc314a7d
minami <sec0415.minami@gmail.com>
2024-03-13 12:15:51 +0000

    02 課題

--- practice.php
+++ practice.php
@@ -18,3 +18,25 @@ echo $hello . $name . $world;
 echo "\n";
 
 # 課題４
+$tech_boost = "tech";
+$tech_boost .="boost";
+echo $tech_boost;
+echo "\n";
+
+#課題５
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+echo $calendar_2018["December"];
